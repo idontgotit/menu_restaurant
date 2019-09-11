cd67a9bbbdf5d7378dcd3c0640daac8489c5a09d
ngocpt <ngoc.pt192@gmail.com>
2019-09-11 14:08:42 +0700

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+
+
+import { Page1Page } from '../pages/page1/page1';
+
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  @ViewChild(Nav) navCtrl: Nav;
+    rootPage:any = Page1Page;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -18,5 +24,8 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+  goToPage1(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(Page1Page);
+  }
 }
-
